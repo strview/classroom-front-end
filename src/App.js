a46c1a86abb7bee4ad2b38a5edb0514347d94fc9
kennyjacobson <kenny.jacobson@gmail.com>
2023-10-22 15:55:06 -0700

    From class Oct 19

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getComplaints } from './helpers/apiEndpoints';
+import { getComplaints, createComplaint } from './helpers/apiEndpoints';
 
 function App() {
 
@@ -29,6 +29,24 @@ function App() {
     fetchComplaints()
   }, [])
 
+//Create 
+const handleSubmit = async (event) => {
+  event.preventDefault()
+  const complaint = {
+    message: event.target.message.value,
+    source: "kenny-front-end",
+  }
+  console.log("complaint", complaint)
+  const { apiEndpoint, apiOptions } = await createComplaint(complaint)
+  console.log("apiEndpoint", apiEndpoint)
+  const response = await fetch(apiEndpoint, apiOptions)
+  console.log("response", response)
+  const data = await response.json()
+  console.log(data)
+  setComplaints([...complaints, data])
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -36,10 +54,53 @@ function App() {
         <p>
           Complaint Portal
         </p>
+        
         <div>
+          <hr></hr>
+          <h2>Submit a complaint</h2>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="message" />
+            <button type="submit">Submit Complaint</button>
+          </form>
+        </div>
+
+        
+
+      <div>
+        <hr/>
+      <h2>Sample Table</h2>
+      <table>
+      <thead>
+        <tr>
+          <th>Month</th>
+          <th>Savings</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>January</td>
+          <td>$100</td>
+        </tr>
+        <tr>
+          <td>February</td>
+          <td>$80</td>
+        </tr>
+      </tbody>
+
+    </table>
+
+      </div>
+
+
+
+
+        <div>
+          <hr/>
+          <h2>Complaints</h2>
           {complaints.map((complaint) => (
             <div key={complaint.id}>
-              <p>{complaint.message}</p>
+              <p>{complaint.message} | {complaint.submitter} | {complaint.source} | {complaint.created_at}</p>
+
             </div>
           ))}
         </div>
