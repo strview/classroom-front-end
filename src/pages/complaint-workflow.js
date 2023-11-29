08f93268cd84597ae0ea3beee27e0e45e9b0f994
ethan <emoua337@gmail.com>
2023-11-28 19:14:22 -0500

    save changes

--- src/pages/complaint-workflow.js
+++ src/pages/complaint-workflow.js
@@ -1,11 +1,47 @@
+//import './workforce.css';
+import '../App.css';
 
 
 const ComplaintWorkflow = () => {
     return (
-        <>
-            <h1>Complaint Workflow</h1>
+        <>  
+        <div className='whole'>
+            <div className='header'>
+                <h1>Complaint Workflow</h1>
+                <small>Powered by Azura</small>
+                <div className='bubble'>EM</div>
+                <div className='log'>
+                    <button>Logout</button>
+                </div>    
+            </div>
+            <div className="therest">
+            <div className="leftside">
+                <h1>this is gonna be the leftside </h1> 
+                <p>
+                    <ul>
+                        <li>New Complaints</li>
+                        <li>Complaints in Progress</li>
+                        <li>Make a Complaint</li>
+                        <li>Forwarded Complaints</li>
+                        <li><a href='https://www.dhcd.virginia.gov/codes' target='_blank'>Code Office</a></li>
+
+                    </ul>
+                </p>
+
+            </div>
+
+            <div className="rightside">
+                <p>I want this to be the rightside. data and stuff is this still wokring</p>
+
+
+            </div>
+            </div>
+        </div>
         </>
+            
     )
 }
+
+
 
 export default ComplaintWorkflow
---
+++ src/pages/state.js
@@ -0,0 +1,10 @@
+export class NewComplaints{
+    constructor(title, content){
+        this.title;
+        this.content;
+    }
+}
+
+const NewComplaints1 = new NewComplaints 
+
+
