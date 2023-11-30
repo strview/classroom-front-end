cdfeae0e5eb06c66c20f7b3264b93f597646ccdb
= <=>
2023-11-30 14:02:52 -0500

    This is the 11/30 progress

--- src/pages/submit-complaint.js
+++ src/pages/submit-complaint.js
@@ -1,4 +1,5 @@
-import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material"
+import { Box, Button, Container, Grid, TextField, Typography, FormControl, InputLabel, Select, MenuItem } from "@mui/material"
+import * as React from 'react'
 import { useState } from "react"
 import { createComplaint } from "../helpers/apiEndpoints"
 
@@ -28,6 +29,86 @@ const SubmitComplaint = () => {
     const clearComplaint = () => {
         setComplaint({})
     }
+
+//This is for the type form dropdown
+    const [type, setFood] = React.useState('noise');
+
+//This is for selecting type form dropdown
+    const handleTypeChange = (event) => {
+
+        setFood(event.target.value);
+
+ };
+
+
+    return (
+        <>
+            <h1>Complaint Form</h1>
+
+
+            <form>
+            <FormControl fullWidth>
+
+            <TextField margin="normal"
+                            required
+                            fullWidth
+                            id="message"
+                            label="Message"
+                            name="message"
+                            autoComplete="message"
+                            autoFocus
+                            value={complaint.message || ""}
+                            onChange={(event) => {setComplaint({...complaint, message: event.target.value})}}
+                            />
+
+
+                <InputLabel id="type-label">Complaint type</InputLabel>
+                <Select
+                    labelId="type-label"
+                    id="type"
+                    value={type}
+                    label="Complaint Type"
+                    onChange={handleTypeChange}
+                >
+                    <MenuItem value="noise">Noise</MenuItem>
+                    <MenuItem value="trash">Trash</MenuItem>
+                    <MenuItem value="parking">Parking</MenuItem>
+                    <MenuItem value="pet">Pet/Pet Waste</MenuItem>
+                    <MenuItem value="safety">Life Safety</MenuItem>
+                    <MenuItem value="fire">Outdoor Fires</MenuItem>
+                    <MenuItem value="night">Night Lighting</MenuItem>
+                    <MenuItem value="hazard">Hazardous/Unsafe Conditions</MenuItem>
+                    <MenuItem value="other">Other</MenuItem>
+                </Select>
+
+                
+            </FormControl>
+            
+            <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2, mr: 2, ml: 2 }}
+                        >
+                            Save
+                        </Button>
+                        <Button
+                            type="button"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2, mr: 2, ml: 2 }}
+                            onClick={clearComplaint}
+                        >
+                            Clear
+                        </Button>
+                    </Box>
+
+                    </form>
+
+        </>
+    )
+    
 }
 
 export default SubmitComplaint
