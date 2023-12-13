8fb9b41cca022427365373c99fe686363430743e
ethan <emoua337@gmail.com>
2023-12-13 13:01:51 -0500

    senior proj final

--- src/pages/complaint-workflow.js
+++ src/pages/complaint-workflow.js
@@ -1,74 +1,54 @@
 import { useRef, useState } from "react"
 import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material"
 import { createComplaint, getComplaint, updateComplaint, getComplaints } from "../helpers/apiEndpoints"
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Autocomplete from '@mui/material/Autocomplete';
 
 
-//const APP_SOURCE = "kenny-front-end" 
+
+
 
 
 const Workflow = () => {
     const [complaint, setComplaint] = useState({})
     const [displayComplaint, setDisplayComplaint] = useState({})
     const [notes, setNotes] = useState("");
-    const [age, setAge] = useState('');
-    const handleChange = (event) => {
-        setAge(event.target.value);
-      };  
-    const [timestamps, setTimestamps] = useState({
-        timeReceived: "",
-        timeNew: "",
-        timeInProgress: "",
-        timeForwarded: "",
-        timeResolved: "",
-    });
+    const [selectedComplaintId, setSelectedComplaintId] = useState(null);
     const multipleComplaints = {
-        displayComplaintId : useRef("cc2ea524-66b5-4d01-8039-897879d77c61"),
-        displayComplaintId2: useRef("another-complaint-id"),
-    }
-    
+        "cc2ea524-66b5-4d01-8039-897879d77c61": useRef("cc2ea524-66b5-4d01-8039-897879d77c61"),
+        "fc41d8e6-5058-46d3-8d4a-4892254454c2": useRef("fc41d8e6-5058-46d3-8d4a-4892254454c2"), 
+        "b5615263-c677-4592-8c6b-ee5ba27ecb42": useRef("b5615263-c677-4592-8c6b-ee5ba27ecb42"),
+        "fe871c91-a983-4168-8c83-52c8f4ca9df1": useRef("fe871c91-a983-4168-8c83-52c8f4ca9df1"),
+        "b8744fd7-1159-414b-908f-3f0cd994b796": useRef("b8744fd7-1159-414b-908f-3f0cd994b796"),
+        "4bb2091e-c820-4da8-8ac9-d5f5f089aefc": useRef("4bb2091e-c820-4da8-8ac9-d5f5f089aefc"),
+        "cc47f81a-0f83-4758-9588-c15415c8b496": useRef("cc47f81a-0f83-4758-9588-c15415c8b496"),
+        "6c137431-0add-4456-9b6e-4738d7aa47a6": useRef("6c137431-0add-4456-9b6e-4738d7aa47a6"),
 
-    const handleSelectChange = async (event) => {
-        const selectedComplaintId = event.target.value;
-        const { apiEndpoint, apiOptions } = await getComplaint(selectedComplaintId);
-        const response = await fetch(apiEndpoint, apiOptions);
-        const data = await response.json();
-        setDisplayComplaint(data);
-        setNotes(""); 
-        setTimestamps({
-            timeReceived: getTimeStamp(),
-            timeNew: "",
-            timeInProgress: "",
-            timeForwarded: "",
-            timeResolved: "",
-        });
     };
+   
 
-    const getSingleComplaint = async () => {
-        const { apiEndpoint, apiOptions } = await getComplaint(multipleComplaints.displayComplaintId.current)
-        console.log("apiEndpoint", apiEndpoint)
-        const response = await fetch(apiEndpoint, apiOptions)
-        console.log("response", response)
-        const data = await response.json()
-        console.log(data)
-        setDisplayComplaint(data)
-        setTimestamps({
-            timeReceived: getTimeStamp(),
-            timeNew: "",
-            timeInProgress: "",
-            timeForwarded: "",
-            timeResolved: "",
-        });
+     const getSingleComplaint = async () => {
+        if (selectedComplaintId) {
+            const { apiEndpoint, apiOptions } = await getComplaint(selectedComplaintId);
+            console.log("apiEndpoint", apiEndpoint)
+            const response = await fetch(apiEndpoint, apiOptions);
+            console.log("response", response)
+            const data = await response.json();
+            console.log(data)
+            setDisplayComplaint(data);
+        }
+        else {
+            alert("Please Enter A Complaint ID")
+        } 
 
-        
+
+
+
     }
 
 
+   
 
-    
+
 
 
     const updateComplaintStatus = async (status) => {
@@ -80,50 +60,74 @@ const Workflow = () => {
         console.log(data)
         setDisplayComplaint(data)
         getSingleComplaint()
-        const updatedTimestamps = { ...timestamps };
-        switch (status) {
-            case 'New':
-                updatedTimestamps.timeNew = getTimeStamp();
-                break;
-            case 'In Process':
-                updatedTimestamps.timeInProgress = getTimeStamp();
-                break;
-            case 'Forwarded: In Process':
-                updatedTimestamps.timeForwarded = getTimeStamp();
-                break;
-            case 'Resolved':
-                updatedTimestamps.timeResolved = getTimeStamp();
-                break;
-            default:
-                break;
-        }
-        setTimestamps(updatedTimestamps);
-    };
         
+    };
+       
+   
+
+
+    const assignToPerson = async () => {
+        const { apiEndpoint, apiOptions } = await updateComplaint(displayComplaint.id, "assigned", displayComplaint.assigned)
+        console.log("apiEndpoint", apiEndpoint)
+        const response = await fetch(apiEndpoint, apiOptions)
+        console.log("response", response)
+        const data = await response.json()
+        console.log(data)
+        setDisplayComplaint(data)
+        getSingleComplaint()
+        
+    };
+
     
-
-
-    function getTimeStamp() {
-        var now = new Date();
-        return ((now.getMonth() + 1) + '/' + (now.getDate()) + '/' + now.getFullYear() + " " + now.getHours() + ':'
-                      + ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' + ((now.getSeconds() < 10) ? ("0" + now
-                      .getSeconds()) : (now.getSeconds())));
- }
  
 
-  
-  
+
+ 
+ 
+
 
     return (
         <>
             <Typography variant="h1">Workflow</Typography>
 
-            
 
             
-            <Typography variant="h4">Complaints Portal</Typography>
+
+
+           
+            <Typography variant="h4" sx={{mb:2}}>Complaints Portal</Typography>
             <Box sx={{mb:4}}>
                 <Box sx={{display: "flex", justifyContent: "space-between"}}>
+
+
+
+
+
+
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    options={Object.keys(multipleComplaints)}
+                    sx={{ width: 2000 }}
+                    renderInput={(params) => <TextField {...params} label="Search for a Complaint ID" />}
+                    onChange={(event, value) => {
+                        setSelectedComplaintId(multipleComplaints[value].current);
+                    }}
+                />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     <Button
                         type="button"
                         fullWidth
@@ -131,32 +135,38 @@ const Workflow = () => {
                         sx={{ mt: 3, mb: 2, mr: 2, ml: 2 }}
                         onClick={() => {getSingleComplaint()}}
                        
-                    >   
+                    >  
                         Get Single Complaint
                     </Button>
 
+                                 
+                </Box>
+                <Box>
+               
+                {displayComplaint.id && (
+                        <>
+                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                            <Typography variant="h6">Id: {displayComplaint.id}</Typography>
+                            
+                        </Box>
+                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                            <Typography variant="h6">Status: {displayComplaint.status}</Typography>
+                            
+                        </Box>
+                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                            <Typography variant="h6">Message: {displayComplaint.message}</Typography>
+                            
+                        </Box>
 
-                    <FormControl fullWidth sx={{ mt: 3, mb: 2, mr: 2, ml: 2 }}>
-                        <InputLabel id="allComplaints">Complaints</InputLabel>
-                        <Select
-                        labelId="allComplaints"
-                        id="allComplaints"
-                        value={displayComplaint.id}
-                        label="Complaints"
-                        onChange={handleSelectChange}
-                        >
-                        <MenuItem value={"cc2ea524-66b5-4d01-8039-897879d77c61"}>cc2ea524-66b5-4d01-8039-897879d77c61</MenuItem>
-                        <MenuItem value={"another-complaint-id"}>another-complaint-id</MenuItem>
-                        </Select>
-                        </FormControl>
-
+                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                            <Typography variant="h6">Assigned to: {displayComplaint.assigned}</Typography>
+                            
+                        </Box>
+                        
                     
-
-
-
-
-
-                    <Button
+                    <Box sx={{mb:4}}>
+                    <Box sx={{display: "flex", justifyContent: "space-between"}}>
+                        <Button
                         type="button"
                         fullWidth
                         variant="contained"
@@ -165,15 +175,6 @@ const Workflow = () => {
                     >
                         Update Status to New
                     </Button>
-                    <Button
-                        type="button"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2, mr: 2, ml: 2 }}
-                        onClick={() => {updateComplaintStatus('In Process')}}
-                    >
-                        Update Status to In Process
-                    </Button>
 
 
                     <Button
@@ -181,15 +182,18 @@ const Workflow = () => {
                         fullWidth
                         variant="contained"
                         sx={{ mt: 3, mb: 2, mr: 2, ml: 2 }}
-                        onClick={() => {updateComplaintStatus('Forwarded: In Process')}}
+                        onClick={() => {updateComplaintStatus('In Progress')}}
                     >
-                        Forward to Code Office
+                        Update Status to In Progress
                     </Button>
 
 
 
+
+
+
                     <Button
-                        type="button"
+                        type="submit"
                         fullWidth
                         variant="contained"
                         sx={{ mt: 3, mb: 2, mr: 2, ml: 2 }}
@@ -197,61 +201,64 @@ const Workflow = () => {
                     >
                         Update Status to Resolved
                     </Button>
-                </Box>
-                <Box>
-                
-                    {displayComplaint.id && (
-                        <>
-                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                            <Typography variant="h6">Id:</Typography>
-                            <Typography variant="h6">{displayComplaint.id}</Typography>
-                        </Box>
-                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                            <Typography variant="h6">Status:</Typography>
-                            <Typography variant="h6">{displayComplaint.status}</Typography>
-                        </Box>
-                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                            <Typography variant="h6">Message:</Typography>
-                            <Typography variant="h6">{displayComplaint.message}</Typography>
-                        </Box>
-                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                            <Typography variant="h6">Time Recieved: {timestamps.timeReceived}</Typography>
-                        </Box>
-                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                            <Typography variant="h6">Time as New: {timestamps.timeNew}</Typography>
-                        </Box>
-                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                            <Typography variant="h6">Time In Progress: {timestamps.timeInProgress}</Typography>
-                        </Box>
-                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                            <Typography variant="h6">Time Forwarded: {timestamps.timeForwarded}</Typography>
-                        </Box>
-                        <Box sx={{display: "flex", justifyContent: "space-between"}}>
-                            <Typography variant="h6">Time Resolved: {timestamps.timeResolved}</Typography>
-                        </Box>
-                    
 
-                        
-                        
-                        
+                   
+
+                    </Box>   
+                       
+                    </Box>
 
 
-                        <TextField
+                       
+                       
+                       <TextField sx={{mb:4}}
+                            id="AssignText"
+                            label="Assignment:"
+                            helperText="Insert Assigned Person"
+                            value={displayComplaint.assigned}
+                            onChange={(event) => {setDisplayComplaint({...displayComplaint, assigned: event.target.value})}}
+                        />
+
+
+                    <Button 
+                        type="button"    
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2, mr: 2, ml: 2 }}
+                        onClick={() => {assignToPerson()}}
+                    >
+                        Press to Assign
+                    </Button>
+
+                            
+                    <Box sx={{mb:4}}>
+                       <TextField 
+                            
                             id="outlined-helperText"
                             label="Notes:"
                             helperText="Insert any special notes"
-                            value={notes} 
+                            value={notes}
                             onChange={(e) => setNotes(e.target.value)}
                         />
-                        </>
+                        </Box>
+                    </>
+                       
                     )}    
-                </Box>
+                    </Box>
+                 
 
-                
+               
     </Box>
  
 
-                
+
+               
+
+
+
+
+
+
+
 
 
 
@@ -260,12 +267,19 @@ const Workflow = () => {
 
 
         </>
-        
+       
     )
    
                     }
 
-                    
+
+                   
+
+
+
+
+
+
 
 
 
@@ -275,3 +289,5 @@ const Workflow = () => {
 export default Workflow
 
 
+
+                
